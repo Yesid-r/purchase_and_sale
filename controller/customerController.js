bbb6ce06e67c706778f984f28af14c378ae96b9b
Yesid Rincon <dairo.rincon@uptc.edu.co>
2024-04-26 14:56:29 -0500

    endpoint update customer

--- controller/customerController.js
+++ controller/customerController.js
@@ -2,31 +2,31 @@ import Customer from "../models/Customer.js";
 import User from "../models/User.js";
 import Delivery from "../models/Delivery.js";
 
-export const createCustomer = async (req, res) =>{
+export const createCustomer = async (req, res) => {
     const user = await User.findById(req.params.userId);
-    
+
     try {
-        const {name, price_per_bottle } = req.body
-        if(!user){
+        const { name, price_per_bottle } = req.body
+        if (!user) {
             res.status(404).json({ success: false, message: "User not found" });
-        }else{
+        } else {
             const newCustomer = new Customer({
                 name,
                 price_per_bottle,
                 user: user._id
             })
             const customerSaved = await newCustomer.save()
-            res.status(201).json({success: true, message:"Customer succesfully created", data: customerSaved})
-        } 
+            res.status(201).json({ success: true, message: "Customer succesfully created", data: customerSaved })
+        }
     } catch (error) {
         console.log(error)
-        res.status(500).json({success: false, message: error.message})
+        res.status(500).json({ success: false, message: error.message })
     }
 
 }
 
 // export const getCustomers = async(req, res) =>{
-     
+
 //     try {
 //         const user = await User.findById(req.params.userId)
 //         if(!user){
@@ -34,12 +34,12 @@ export const createCustomer = async (req, res) =>{
 //         }else{
 //             const customers = await Customer.find({ user: req.params.userId });
 
-            
+
 //             const customersWithDeliveries = await Promise.all(customers.map(async (customer) => {
 //                 const deliveries = await Delivery.find({ customer: customer._id });
 //                 return { customer, deliveries };
 //             }));
-    
+
 //             res.status(200).json({ success: true, message: "Successfully searched", data: customersWithDeliveries });
 //         }    
 
@@ -47,75 +47,91 @@ export const createCustomer = async (req, res) =>{
 //         res.status(500).json({ success: false, message: error.message });
 //     }
 // }
-export const getCustomers = async(req, res) =>{
-     
+export const getCustomers = async (req, res) => {
+
     try {
         const user = await User.findById(req.params.userId)
-        if(!user){
-            res.status(404).json({success: false, message:"User not found"})
-        }else{
+        if (!user) {
+            res.status(404).json({ success: false, message: "User not found" })
+        } else {
             const startDate = req.query.startDate;
             const endDate = req.query.endDate;
 
             const customers = await Customer.find();
 
             const customersWithDeliveries = await Promise.all(customers.map(async (customer) => {
-                const deliveries = await Delivery.find({ 
+                const deliveries = await Delivery.find({
                     customer: customer._id,
-                    date: { $gte: startDate, $lte: endDate } 
+                    date: { $gte: startDate, $lte: endDate }
                 });
                 return { customer, deliveries };
             }));
-    
+
             res.status(200).json({ success: true, message: "Successfully searched", data: customersWithDeliveries });
-        }    
+        }
 
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
     }
 }
 
-export const deleteCustomer = async(req, res) =>{
+
+export const deleteCustomer = async (req, res) => {
     try {
         const customerId = req.params.customerId
         const customer = await Customer.findByIdAndDelete(customerId)
-        if(!customer){
-            return res.status(404).json({success: false, message:"customer not found"})
-        }else{
-            return res.status(200).json({success: true, message: "customer delete succesfully", data: customer })
+        if (!customer) {
+            return res.status(404).json({ success: false, message: "customer not found" })
+        } else {
+            return res.status(200).json({ success: true, message: "customer delete succesfully", data: customer })
         }
 
     } catch (error) {
-        
+
     }
 }
-export const getCustomer = async(req, res) =>{
+export const updateCustomer = async (req, res) => {
+    try {
+        const { customer_id, name, price_per_bottle } = req.body
+        const customer = await Customer.findById(customer_id)
+        if (!customer) {
+            return res.status(404).json({ success: false, message: "customer not found" })
+        } else {
+
+            const customer_update = await Customer.findByIdAndUpdate({ _id: customer_id }, {name: name, price_per_bottle: price_per_bottle})
+            res.status(200).json({success:true, message:"customer update", data:customer_update })
+        }
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
+export const getCustomer = async (req, res) => {
     try {
         const customerId = req.params.customerId
         const customer = await Customer.findById(customerId)
-        if(!customer){
-            return res.status(404).json({success: false, message:"customer not found"})
-        }else{
-            return res.status(200).json({success: true, message: "customer succesfully found", data: customer })
+        if (!customer) {
+            return res.status(404).json({ success: false, message: "customer not found" })
+        } else {
+            return res.status(200).json({ success: true, message: "customer succesfully found", data: customer })
         }
     } catch (error) {
-        
+
     }
 }
 
 export const getCustomersOnDateWithoutDelivery = async (req, res) => {
     try {
-        const { userId } = req.params; 
-        const {date} = req.query;
+        const { userId } = req.params;
+        const { date } = req.query;
         const customers = await Customer.find();
         let undeliveredCustomers = [];
         for (const customer of customers) {
-            
-            const deliveries = await Delivery.find({ 
+
+            const deliveries = await Delivery.find({
                 customer: customer._id,
                 date: new Date(date)
             });
-            if(deliveries.length === 0) {
+            if (deliveries.length === 0) {
                 undeliveredCustomers.push(customer);
             }
         }
--- routes/customer.js
+++ routes/customer.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {createCustomer, deleteCustomer, getCustomer, getCustomers, getCustomersOnDateWithoutDelivery} from './../controller/customerController.js'
+import {createCustomer, deleteCustomer, getCustomer, getCustomers, getCustomersOnDateWithoutDelivery, updateCustomer} from './../controller/customerController.js'
 
 
 const router = express.Router()
@@ -9,5 +9,6 @@ router.get('/getCustomers/:userId', getCustomers)
 router.delete('/deleteCustomer/:customerId', deleteCustomer)
 router.get('/getCustomer/:customerId', getCustomer)
 router.get('/customers/:userId', getCustomersOnDateWithoutDelivery)
+router.put('/update-customer/:userId', updateCustomer)
 
 export default router
